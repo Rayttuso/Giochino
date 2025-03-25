93071ce8d3507598c4ffd86691b9eabbfb100356
andre0660 <andrea.becciu@studenti.galileilivorno.edu.it>
2025-03-25 16:36:52 +0100

    file input player

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -3,40 +3,4 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0.005f;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKey("d")){
-            float translation = Input.GetAxis("Horizontal")*speed;
-             transform.Translate(translation, 0, 0);
-
-        }
-        
-        else if(Input.GetKey("a")){
-            float translation = Input.GetAxis("Horizontal")*speed;
-             transform.Translate(translation, 0, 0);
-
-        }
-
-        else if(Input.GetKey("w")){
-            float translation = Input.GetAxis("Vertical")*speed;
-             transform.Translate(0, translation, 0);
-
-        }
-
-        else if(Input.GetKey("s")){
-            float translation = Input.GetAxis("Vertical")*speed;
-             transform.Translate(0, translation, 0);
-
-        }
-
-    }
 }
--- Assets/Scripts/PlayerScripts/PlayerMovimentStats.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovimentStats.cs
@@ -1,16 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovimentStats : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(menuName="Player Movement")]
+public class PlayerMovimentStats : ScriptableObject
+{
+    [Header("Walk")] 
+    [Range(1f,100f)] public float MaxWalkSpeed=12.5f;
+    [Range(0.25f,50f)] public float GroundAccelleration=5f;
+    [Range(0.25f,50f)] public float GroundDecelleration=20f;
+    [Range(0.25f,50f)] public float AirAccelleration=5f;
+    [Range(0.25f,50f)] public float AirDecelleration=5f;
+
+    [Header("Run")] 
+    [Range(1f,100f)] public float MaxRunSpeed=20f;
+
+    [Header("Grounded/Collision Checks")] 
+    public LayerMask GroundLayer;
+    public float GroundDetectionRayLength=0.02f;
+    public float HeadDetectionRayLength=0.02f;
+    [Range(0f,1f)] public float HeadWidth=0.75f;
 }
