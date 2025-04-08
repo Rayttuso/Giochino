83124c43d179f40c588ac25d7c8919190dad6c4b
Rayttuso <gabroelebessi07@gmail.com>
2025-04-08 18:01:39 +0200

    Modifiche piattaforme (Incomplete)

--- Assets/Scripts/Enviroment/Platforms.cs
+++ Assets/Scripts/Enviroment/Platforms.cs
@@ -3,24 +3,55 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Platforms : MonoBehaviour{
-    private bool isOnPlatform;
-    private float _timerDiscesa;
+    public bool isOnPlatform;
+    public bool isFalling;
+
+    private GameObject temp;
 
     void Update(){
-        if(Input.GetKey(KeyCode.S) && isOnPlatform==true){
-            transform.GetComponent<PlatformEffector2D>().rotationalOffset = 180;
-            _timerDiscesa = 1f;
-        }
-
-        else{
-            transform.GetComponent<PlatformEffector2D>().rotationalOffset = 0;
+        /*
+        if(Input.GetKey(KeyCode.S) && isOnPlatform==true && isFalling==false){
+            //transform.GetComponent<PlatformEffector2D>().rotationalOffset = 180;
             isOnPlatform=false;
+            isFalling=true;
+            temp.SetActive(false);
         }
+        */
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        temp=this.gameObject;
+
+        if(this.gameObject != temp){
+            temp.SetActive(true);
+        }
+        
     }
 
     private void OnCollisionStay2D(Collision2D collision){
-        if(collision.gameObject.tag=="Player"){
+        if(collision.gameObject.GetComponentInChildren<Collider2D>().tag == "Feet" && isFalling==false){
             isOnPlatform=true;
+
         }
+
+        if(Input.GetKey(KeyCode.S) && isOnPlatform==true && isFalling==false){
+            //transform.GetComponent<PlatformEffector2D>().rotationalOffset = 180;
+            isOnPlatform=false;
+            isFalling=true;
+            temp.SetActive(false);
+        }
+
+
+        
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        transform.GetComponent<PlatformEffector2D>().rotationalOffset = 0;
+        isFalling=false;
+        isOnPlatform=false;
+    }
+
 }
